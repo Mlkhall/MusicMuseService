ec2e5b4ccc7ba519245719f01075f6af137f01c0
chumakov-md <chumakov-md@zvuk.com>
2024-11-06 02:38:58 +0300

    fix docs 1

--- music_muse/settings.py
+++ music_muse/settings.py
@@ -152,7 +152,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
