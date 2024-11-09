453fa985974d76dbc8b2aced68b30e5fcd70db3b
chumakov-md <chumakov-md@zvuk.com>
2024-11-09 22:51:08 +0300

    исправления токена авторизации

--- music_muse/settings.py
+++ music_muse/settings.py
@@ -177,6 +177,11 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     "PAGE_SIZE": 10,
 }
 
